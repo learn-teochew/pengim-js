9cddb4fe7dac05b2e6d65fdedb16d231d55781fb
kbseah <kb.seah@gmail.com>
2024-12-17 12:26:30 +0800

    Support GGN conversion in web page

--- webscript.js
+++ webscript.js
@@ -5,19 +5,21 @@ let textarea1 = document.getElementById("input");
 let textarea2 = document.getElementById("output");
 
 // first button element
-let button1 = document.getElementById("gdpi2puj_button");
+let button1 = document.getElementById("topuj_button");
 
 button1.addEventListener("click", function() {
   let input = textarea1.value;
-  let convertedLine = convertLine(input, "toPuj", "gdpi");
+  let system = document.querySelector('input[name="system"]:checked').value;
+  let convertedLine = convertLine(input, "toPuj", system);
   textarea2.value = convertedLine;
 });
 
 // second button element
-let button2 = document.getElementById("puj2gdpi_button");
+let button2 = document.getElementById("frompuj_button");
 
 button2.addEventListener("click", function() {
   let input = textarea1.value;
-  let convertedLine = convertLine(input, "fromPuj", "gdpi");
+  let system = document.querySelector('input[name="system"]:checked').value;
+  let convertedLine = convertLine(input, "fromPuj", system);
   textarea2.value = convertedLine;
 });
