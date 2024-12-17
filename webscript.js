c46689dc9814beac152d6641cd2a685e0b4fca12
kbseah <kb.seah@gmail.com>
2024-12-17 10:01:33 +0800

    Web page styling

--- webscript.js
+++ webscript.js
@@ -1,14 +1,14 @@
 import { convertLine } from "./src.js";
 
 // Get the textarea elements
-let textarea = document.getElementById("input");
+let input = document.getElementById("input");
 let output = document.getElementById("output");
 
 // first button element
 let button1 = document.getElementById("gdpi2puj_button");
 
 button1.addEventListener("click", function() {
-  let input = textarea.value;
+  let input = input.value;
   let convertedLine = convertLine(input, "gdpi2puj");
   output.value = convertedLine;
 });
@@ -17,7 +17,7 @@ button1.addEventListener("click", function() {
 let button2 = document.getElementById("puj2gdpi_button");
 
 button2.addEventListener("click", function() {
-  let input = textarea.value;
+  let input = output.value;
   let convertedLine = convertLine(input, "puj2gdpi");
-  output.value = convertedLine;
+  input.value = convertedLine;
 });
