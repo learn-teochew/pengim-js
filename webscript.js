6bc9203f3fac5468b22aacf77be9cfbce40fdf3f
kbseah <kb.seah@gmail.com>
2024-12-17 10:14:46 +0800

    Rename to avoid name clash

--- webscript.js
+++ webscript.js
@@ -1,23 +1,23 @@
 import { convertLine } from "./src.js";
 
 // Get the textarea elements
-let input = document.getElementById("input");
-let output = document.getElementById("output");
+let textarea1 = document.getElementById("input");
+let textarea2 = document.getElementById("output");
 
 // first button element
 let button1 = document.getElementById("gdpi2puj_button");
 
 button1.addEventListener("click", function() {
-  let input = input.value;
+  let input = textarea1.value;
   let convertedLine = convertLine(input, "gdpi2puj");
-  output.value = convertedLine;
+  textarea2.value = convertedLine;
 });
 
 // second button element
 let button2 = document.getElementById("puj2gdpi_button");
 
 button2.addEventListener("click", function() {
-  let input = output.value;
+  let input = textarea1.value;
   let convertedLine = convertLine(input, "puj2gdpi");
-  input.value = convertedLine;
+  textarea2.value = convertedLine;
 });
