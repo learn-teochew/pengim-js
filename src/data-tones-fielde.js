ee6c450b20ea031337a76a1ada7c4ca4c58ba11e
kbseah <kb.seah@gmail.com>
2024-12-19 09:09:56 +0800

    Fix wrong tone 6 diacritic

--- src/data-tones-fielde.js
+++ src/data-tones-fielde.js
@@ -1,7 +1,7 @@
 export const toneCodeToNumber = {
   0x301 : 2,
   0x300 : 3,
-  0x302 : 58,
+  0x302 : 58, // Ambiguous, depends on coda
   0x306 : 6,
   0x30C : 6,
   0x304 : 7,
@@ -12,7 +12,7 @@ export const toneNumberToCode = {
   2 : 0x301 ,
   3 : 0x300 ,
   5 : 0x302 ,
-  6 : 0x303 ,
+  6 : 0x306 ,
   7 : 0x304 ,
   8 : 0x302
 };
